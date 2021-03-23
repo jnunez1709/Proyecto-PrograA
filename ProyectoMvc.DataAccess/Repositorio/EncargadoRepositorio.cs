07ddbebe5979538bc79bdba2c6bf4d7925fc6b9d
Geison Eduardo <gvillalobos40174@ufide.ac.cr>
2021-03-22 22:01:45 -0600

    Agregar controlador y repositorios

---
+++ ProyectoMvc.DataAccess/Repositorio/EncargadoRepositorio.cs
@@ -0,0 +1,33 @@
+﻿using ProyectoMvc.DataAccess.Data;
+using ProyectoMvc.DataAccess.Repositorio.IRepositorio;
+using ProyectoMvc.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ProyectoMvc.DataAccess.Repositorio
+{
+    public class EncargadoRepositorio : Repositorio<Encargado>, IEncargadoRepositorio
+    {
+        public EncargadoRepositorio(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+
+        readonly ApplicationDbContext _db;
+
+        public void Actualizar(Encargado encargado)
+        {
+            var t = _db.Encargados.FirstOrDefault(s => s.Id == encargado.Id);
+            if (t != null)
+            {
+                t.Nombre = encargado.Nombre;
+                t.Apellidos = encargado.Apellidos;
+                t.TipoIdentificacionId = encargado.TipoIdentificacionId;
+                t.Identificacion = encargado.Identificacion;
+                t.FechaNacimiento = encargado.FechaNacimiento;
+            }
+        }
+    }
+}
---
+++ ProyectoMvc.DataAccess/Repositorio/IRepositorio/IControlador.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProyectoMvc.DataAccess.Repositorio.IRepositorio
+{
+    public interface IControlador : IDisposable
+    {
+        //IExpedienteRepositorio Expedientes { get; }
+
+        ITipoIdentificacionRepositorio TiposIdentificacion { get; }
+
+        IEncargadoRepositorio Encargados { get; }
+
+        IEmpleadoRepositorio Empleados { get; }
+
+        void Guardar();
+    }
+}
