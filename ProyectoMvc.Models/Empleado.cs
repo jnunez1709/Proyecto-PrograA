5ca02047f8f833d9c63d235577e129dee3864824
eri1200 <60492573+eri1200@users.noreply.github.com>
2021-03-22 20:46:55 -0600

    Agrego de clases Encargado y Empleado

---
+++ ProyectoMvc.Models/Empleado.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace ProyectoMvc.Models
+{
+    public class Empleado
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [DisplayName("Identificación")]
+        [Required(ErrorMessage = "Este campo es requerido.")]
+        [MaxLength(20, ErrorMessage = "Máximo 20 caracteres.")]
+        public string Identificacion { get; set; }
+        public int IdentificacionId { get; set; }
+
+        [ForeignKey("IdentificacionId")]
+
+        [Required]
+        [DisplayName("Nombre")]
+
+        public string Nombre { get; set; }
+
+        [Required]
+        [DisplayName("Apellidos")]
+
+        public string Apellidos { get; set; }
+
+        [Required]
+        [DisplayName("Telefono")]
+        public string Telefono { get; set; }
+
+        [Required]
+        [DisplayName("Correo Electronico")]
+
+        public string Correo { get; set; }
+
+
+        [DisplayFormat(DataFormatString = "{0:MM/dd/yyyy}")]
+        public DateTime FechaNacimiento { get; set; }
+
+        public int Horaslaborales { get; set; }
+
+
+    }
+}
---
+++ ProyectoMvc.Models/Encargado.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ProyectoMvc.Models
+{
+    public class Encargado
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [DisplayName("Identificación")]
+        [Required(ErrorMessage = "Este campo es requerido.")]
+        [MaxLength(20, ErrorMessage = "Máximo 20 caracteres.")]
+        public string Identificacion { get; set; }
+        public int IdentificacionId { get; set; }
+
+        [ForeignKey("IdentificacionId")]
+
+        [Required]
+        [DisplayName("Nombre")]
+
+        public string Nombre { get; set; }
+
+        [Required]
+        [DisplayName("Apellidos")]
+
+        public string Apellidos { get; set; }
+
+        [Required]
+        [DisplayName("Telefono")]
+        public string Telefono { get; set; }
+
+        [Required]
+        [DisplayName("Correo Electronico")]
+
+        public string Correo { get; set; }
+
+        [Required]
+        [DisplayName("Tipo de Parentesco")]
+
+        public string Parentesco { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:MM/dd/yyyy}")]
+        public DateTime FechaNacimiento { get; set; }
+
+
+
+    }
+}
